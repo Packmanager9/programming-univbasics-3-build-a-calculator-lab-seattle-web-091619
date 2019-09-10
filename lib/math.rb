facb68bd09456ab3557101e05249413aa5ba6cad
Cole Ditzler <colejditzler@gmail.com>
2019-09-10 22:42:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -18,15 +18,3 @@ def modulo(num1, num2)
 num1%num2
 end
 
-def square_root(num)
-deck = 1;
-holder = 0
-for deck <= (num/deck) do
-holder=(num/deck)
-for holder == deck do
-  return deck
-end
-end
-
-
-end
